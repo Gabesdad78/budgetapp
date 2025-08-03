fe04e9db0c697e91ba94f4c6dbc5d1f05e32b373
David Ruhnkegit <davidaruhnke1978@gmail.com>
2025-08-03 16:56:14 -0700

    Fix Python 3.12 compatibility for Vercel deployment

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ ml_model = MLModel()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 @app.route('/')
 def index():
